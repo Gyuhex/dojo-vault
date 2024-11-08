a0519d7ba1c693f5f407dffab2bc11e4adbc1d70
Gyuhex <gyuhexmeow7@gmail.com>
2024-11-08 20:53:18 +0800

    Add new student form on add-student.html and initial css design from form.css

---
+++ assets/js/addStudent.js
@@ -0,0 +1,17 @@
+document.getElementById('addStudentForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from submitting the traditional way
+
+    // Get input Values
+    const studentName = document.getElementById('studentName').value;
+    const age = document.getElementById('age').value;
+    const address = document.getElementById('address').value;
+    const contactNumber = document.getElementById('contactNumber').value;
+    const membership = document.getElementById('membership').value;
+    const beltColor = document.getElementById('beltColor').value;
+
+    // console.log(studentName, age, address, contactNumber, membership, beltColor);
+
+    const student = {
+        
+    }
+});
