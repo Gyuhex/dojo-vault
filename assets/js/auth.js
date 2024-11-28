d21372dde897091a9b25677fee0f610cd9544fa8
Gyuhex <gyuhexmeow7@gmail.com>
2024-11-28 22:30:00 +0800

    Refactor auth.js to OOP

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,38 +1,41 @@
+
+class Auth {
+    constructor(email, password) {
+        this.email = email
+        this.password = password
+    }
+
+    loginAuth = () => {
+         // Get form values
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Simple Validation for hardcoded credentials (for now)
+        if (email === email && password === password) {
+            alert("Login Success");
+
+            // Save session for this user (hardcoded for now)
+            const user = {
+                email: email,
+                password: password,
+                students: [] // Initialize with an empty student list
+            };
+
+            // Store user session in localStorage
+            localStorage.setItem('currentUser', JSON.stringify(user));
+
+            // Redirect to the dashboard
+            window.location.href = 'dashboard.html';
+        } else {
+            alert("Invalid credentials");
+        }
+    }
+}
+
+let authenticate = new Auth();
+
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Get form values
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    // Simple Validation for hardcoded credentials (for now)
-    if (email === email && password === password) {
-        alert("Login Success");
-
-        // Save session for this user (hardcoded for now)
-        const user = {
-            email: email,
-            password: password,
-            students: [] // Initialize with an empty student list
-        };
-
-        // Store user session in localStorage
-        localStorage.setItem('currentUser', JSON.stringify(user));
-
-        // Redirect to the dashboard
-        window.location.href = 'dashboard.html';
-    } else {
-        alert("Invalid credentials");
-    }
-
-    // Check credentials in localStorage (for demo purposes)
-    // const users = JSON.parse(localStorage.getItem('users')) || [];
-    // const user = users.find(u => u.email === email && u.password === password);
-
-    // if (user) {
-    //     localStorage.setItem('currentUser', JSON.stringify(user));
-    //     window.location.href = 'dashboard.html';
-    // } else {
-    //     alert('Invalid credentials');
-    // }
+    authenticate.loginAuth();
 });
